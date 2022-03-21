431ebcef5c607623016bdc7e29eaa8d9650039f5
karagoz-ziya <101717928+karagoz-ziya@users.noreply.github.com>
2022-03-21 15:36:26 +0300

    google sign in eklendi

--- app/src/main/java/com/example/trashcanapp/MainActivity.java
+++ app/src/main/java/com/example/trashcanapp/MainActivity.java
@@ -1,28 +1,52 @@
 package com.example.trashcanapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+public class MainActivity extends AppCompatActivity  {
 
-public class MainActivity extends AppCompatActivity {
+    // TAGS
+    private static final String TAG = "GOOGLE_SING_IN_TAG";
+   // Button button;
+   SignInButton sign_in_button;
 
-    Button button;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "girdi");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.button);
 
+        sign_in_button = findViewById(R.id.sign_in_button);
+
+    gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+            .requestEmail()
+            .build();
+
+    gsc = GoogleSignIn.getClient(this, gso);
+        sign_in_button.setOnClickListener(view -> {
+        SignIn();
+    });
+        // Configuring map
+        /*button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,6 +54,33 @@ public class MainActivity extends AppCompatActivity {
                 intent.setClass(getApplicationContext(), MapsActivity.class);
                 startActivity(intent);
             }
-        });
+        });*/
+    }
+
+    private void SignIn() {
+        Intent intent = gsc.getSignInIntent();
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                task.getResult(ApiException.class);
+                HomeActivity();
+            } catch (ApiException e) {
+                Log.i(TAG, String.valueOf(e));
+                Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    private void HomeActivity(){
+        finish();
+        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+        startActivity(intent);
     }
 }
