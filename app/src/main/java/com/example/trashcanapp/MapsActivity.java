d9e37d440a8d963cbfb6081f5b43319c89a02025
karagoz-ziya <sar581999@gmail.com>
2022-04-27 14:26:06 +0300

    a

--- app/src/main/java/com/example/trashcanapp/MapsActivity.java
+++ app/src/main/java/com/example/trashcanapp/MapsActivity.java
@@ -2,6 +2,8 @@ package com.example.trashcanapp;
 
 
 
+import static com.example.trashcanapp.constants.CONSTANTS.binTypeArray;
+
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -12,14 +14,19 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -28,6 +35,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.example.trashcanapp.dbmodel.RecycleBin;
 import com.github.clans.fab.FloatingActionButton;
 import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -45,16 +53,36 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-
+    private FirebaseFirestore db;
     private GoogleMap mMap;
     private static  final String TAG = "MAP_TEST";
     private boolean isPermissionGranted = false;
+    private boolean isCameraPermissionGranted = false;
+    private static final int pic_id = 1;
 
     FloatingActionButton saveButton;
+    TextView textView;
+    Button submitButton;
+    EditText description;
+    Button camera_open_id;
+    ImageView click_image_id;
+
+
+    boolean[] selectedBinType;
+    ArrayList<Integer> binList = new ArrayList<>();
+
+
 
     // Location Finder Objects
     FusedLocationProviderClient fusedClient;
@@ -98,11 +126,18 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 boolean focusable = true; // lets taps outside the popup also dismiss it
                 final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
 
+                // access to the elements of popup window
+                submitButton = popupView.findViewById(R.id.submitBin);
+                textView = popupView.findViewById(R.id.tv_bin);
+                description = popupView.findViewById(R.id.description);
+                camera_open_id = (Button)findViewById(R.id.camera_button);
+                click_image_id = (ImageView)findViewById(R.id.click_image);
+
+                selectedBinType = new boolean[binTypeArray.length];
                 // show the popup window
                 // which view you pass in doesn't matter, it is only used for the window tolken
                 popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
-
-                // dismiss the popup window when touched
+                // can be used for doing some stuff when touching the popup button
                 popupView.setOnTouchListener(new View.OnTouchListener() {
                     @Override
                     public boolean onTouch(View v, MotionEvent event) {
@@ -110,9 +145,105 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                         return true;
                     }
                 });
+                textView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
 
+                        // Initialize alert dialog
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MapsActivity.this);
+
+                        // set title
+                        builder.setTitle("Select Bin Type");
+
+                        // set dialog non cancelable
+                        builder.setCancelable(false);
+
+                        builder.setMultiChoiceItems(binTypeArray, selectedBinType, new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                                // check condition
+                                if (b) {
+                                    // when checkbox selected
+                                    // Add position  in lang list
+                                    binList.add(i);
+                                    // Sort array list
+                                    Collections.sort(binList);
+                                } else {
+                                    // when checkbox unselected
+                                    // Remove position from binList
+                                    binList.remove(Integer.valueOf(i));
+                                }
+                            }
+                        });
+
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // Initialize string builder
+                                StringBuilder stringBuilder = new StringBuilder();
+                                // use for loop
+                                for (int j = 0; j < binList.size(); j++) {
+                                    // concat array value
+                                    stringBuilder.append(binTypeArray[binList.get(j)]);
+                                    // check condition
+                                    if (j != binList.size() - 1) {
+                                        // When j value  not equal
+                                        // to lang list size - 1
+                                        // add comma
+                                        stringBuilder.append(", ");
+                                    }
+                                }
+                                // set text on textView
+                                textView.setText(stringBuilder.toString());
+
+                            }
+                        });
+
+                        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // dismiss dialog
+                                dialogInterface.dismiss();
+                                Log.i(TAG, binList.toString());
+                            }
+                        });
+                        builder.setNeutralButton("Clear All", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // use for loop
+                                for (int j = 0; j < selectedBinType.length; j++) {
+                                    // remove all selection
+                                    selectedBinType[j] = false;
+                                    // clear language list
+                                    binList.clear();
+                                    // clear text view value
+                                    textView.setText("");
+                                }
+
+                            }
+                        });
+                        // show dialog
+                        builder.show();
+
+                    }
+                });
+
+                submitButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Log.i(TAG, binList.toString());
+                        Log.i(TAG, description.getText().toString());
+                        AddRecycleBinToDB(binList, description.getText().toString());
+                    }
+                });
             }
         });
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
     }
 
     private void requestGPSPermission() {
@@ -251,8 +382,48 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 return false;
             }
         });
-
-
     }
+
+    private void AddRecycleBinToDB(ArrayList<Integer> binlist, String description) {
+        db = FirebaseFirestore.getInstance();
+
+        // Setting the object which will send to db with informations given by he user
+        RecycleBin recycleBin = new RecycleBin();
+        recycleBin.setDescription(description);
+        recycleBin.setGeopoint(new GeoPoint(currentLoc.getLatitude(),currentLoc.getLongitude()));
+
+        CollectionReference dbBin = db.collection("RecycleBin");
+        dbBin.add(recycleBin).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+
+                Toast.makeText(MapsActivity.this, "Product Added", Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MapsActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    private void allowCamera() {
+        if(isCameraPermissionGranted){
+            camera_open_id.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v)
+                {
+                    // Create the camera_intent ACTION_IMAGE_CAPTURE
+                    // it will open the camera for capture the image
+                    Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                    // Start the activity with camera_intent,
+                    // and request pic id
+                    startActivityForResult(camera_intent, pic_id);
+                }
+            });
+        }
+    }
+
 }
 
